666032cca42636b4c05ad4223be6029194114a10
RoyPy <bogush.vano@gmail.com>
2017-03-16 20:56:10 +0700

    Fix indents

--- LoC/tests/loc/StatisticsCollectorTest.java
+++ LoC/tests/loc/StatisticsCollectorTest.java
@@ -11,31 +11,31 @@ import java.nio.file.Paths;
 import static org.junit.Assert.*;
 
 public class StatisticsCollectorTest {
-    @Test (expected = StatisticsCollector.NoFilterException.class)
-    public void noFilter() throws Exception {
-        new StatisticsCollector(new Statistics(), new Filter[0]);
-    }
+	@Test (expected = StatisticsCollector.NoFilterException.class)
+	public void noFilter() throws Exception {
+		new StatisticsCollector(new Statistics(), new Filter[0]);
+	}
 
-    @Test
-    public void custom() throws Exception {
-        Statistics statistics = new Statistics();
-        Filter[] filters = ConfigFileReader.readFilters(Paths.get("input/filters"));
-        StatisticsCollector collector = new StatisticsCollector(statistics, filters);
-        Files.walkFileTree(Paths.get("input/dir"), collector);
-        assertEquals(4, statistics.totalFiles());
-        assertEquals(10, statistics.getTotalLines());
+	@Test
+	public void custom() throws Exception {
+		Statistics statistics = new Statistics();
+		Filter[] filters = ConfigFileReader.readFilters(Paths.get("input/filters"));
+		StatisticsCollector collector = new StatisticsCollector(statistics, filters);
+		Files.walkFileTree(Paths.get("input/dir"), collector);
+		assertEquals(4, statistics.totalFiles());
+		assertEquals(10, statistics.getTotalLines());
 
-        assertEquals(1, statistics.getFilterFileStatisticsMap().get(filters[0]).fileCount); // .cpp
-        assertEquals(3, statistics.getFilterFileStatisticsMap().get(filters[0]).lineCount);
-        assertEquals(1, statistics.getFilterFileStatisticsMap().get(filters[1]).fileCount); // .java
-        assertEquals(4, statistics.getFilterFileStatisticsMap().get(filters[1]).lineCount);
-        assertEquals(2, statistics.getFilterFileStatisticsMap().get(filters[2]).fileCount); // Or(.cpp, .java)
-        assertEquals(7, statistics.getFilterFileStatisticsMap().get(filters[2]).lineCount);
-        assertEquals(0, statistics.getFilterFileStatisticsMap().get(filters[3]).fileCount); // And(.cpp, .java)
-        assertEquals(0, statistics.getFilterFileStatisticsMap().get(filters[3]).lineCount);
-        assertEquals(10, statistics.getFilterFileStatisticsMap().get(filters[4]).fileCount); // >1287311235
-        assertEquals(10, statistics.getFilterFileStatisticsMap().get(filters[4]).lineCount);
-        assertEquals(0, statistics.getFilterFileStatisticsMap().get(filters[5]).fileCount); // <1287311235
-        assertEquals(0, statistics.getFilterFileStatisticsMap().get(filters[5]).lineCount);
-    }
+		assertEquals(1, statistics.getFilterFileStatisticsMap().get(filters[0]).fileCount); // .cpp
+		assertEquals(3, statistics.getFilterFileStatisticsMap().get(filters[0]).lineCount);
+		assertEquals(1, statistics.getFilterFileStatisticsMap().get(filters[1]).fileCount); // .java
+		assertEquals(4, statistics.getFilterFileStatisticsMap().get(filters[1]).lineCount);
+		assertEquals(2, statistics.getFilterFileStatisticsMap().get(filters[2]).fileCount); // |(.cpp .java)
+		assertEquals(7, statistics.getFilterFileStatisticsMap().get(filters[2]).lineCount);
+		assertEquals(0, statistics.getFilterFileStatisticsMap().get(filters[3]).fileCount); // &(.cpp .java)
+		assertEquals(0, statistics.getFilterFileStatisticsMap().get(filters[3]).lineCount);
+		assertEquals(10, statistics.getFilterFileStatisticsMap().get(filters[4]).fileCount);// >1287311235
+		assertEquals(10, statistics.getFilterFileStatisticsMap().get(filters[4]).lineCount);
+		assertEquals(0, statistics.getFilterFileStatisticsMap().get(filters[5]).fileCount); // <1287311235
+		assertEquals(0, statistics.getFilterFileStatisticsMap().get(filters[5]).lineCount);
+	}
 }
