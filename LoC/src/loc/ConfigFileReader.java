d6dce45aea8eda8ec3ba64455aaa55af3385c638
RoyPy <bogush.vano@gmail.com>
2017-03-18 00:17:20 +0700

    Remove try-catch block and use throws instead

--- LoC/src/loc/ConfigFileReader.java
+++ LoC/src/loc/ConfigFileReader.java
@@ -5,20 +5,18 @@ import loc.filter.FilterFactory;
 
 import java.io.BufferedReader;
 import java.io.FileReader;
+import java.io.IOException;
 import java.nio.file.Path;
 import java.util.ArrayList;
 import java.util.List;
 
 public class ConfigFileReader {
-	public static Filter[] readFilters(Path filePath) {
+	public static Filter[] readFilters(Path filePath) throws IOException {
 		List<Filter> filterList = new ArrayList<>();
-		try(BufferedReader bufferedReader = new BufferedReader(new FileReader(filePath.toFile()))) {
-			String line;
-			while ((line = bufferedReader.readLine()) != null) {
-				filterList.add(FilterFactory.create(line));
-			}
-		} catch (Exception e) {
-			e.printStackTrace();
+		BufferedReader bufferedReader = new BufferedReader(new FileReader(filePath.toFile()));
+		String line;
+		while ((line = bufferedReader.readLine()) != null) {
+			filterList.add(FilterFactory.create(line));
 		}
 		return filterList.toArray(new Filter[filterList.size()]);
 	}
