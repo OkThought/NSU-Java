0ac0abf403fcc2f1d57c4e69ec9b4a483a383acf
RoyPy <bogush.vano@gmail.com>
2017-04-28 13:36:31 +0700

    Remove unused assignment

--- CarFactory/src/ru/nsu/ccfit/bogush/Main.java
+++ CarFactory/src/ru/nsu/ccfit/bogush/Main.java
@@ -16,7 +16,7 @@ public class Main {
 	public static void main(String[] args) {
 		try {
 			prepare(CONFIG_FILE);
-			FactoryView view = new FactoryView(model);
+			new FactoryView(model);
 		} catch (Exception e) {
 			e.printStackTrace();
 		}
