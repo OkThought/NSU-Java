6827d4e2a285904aa0c68363f6747bab98e6eb0d
RoyPy <bogush.vano@gmail.com>
2017-05-24 12:20:08 +0700

    Change default close operation of main window to "dispose" Add windowClosed listener which interrupts all model threads

--- CarFactory/src/ru/nsu/ccfit/bogush/view/FactoryView.java
+++ CarFactory/src/ru/nsu/ccfit/bogush/view/FactoryView.java
@@ -1,8 +1,11 @@
 package ru.nsu.ccfit.bogush.view;
 
 import ru.nsu.ccfit.bogush.CarFactoryModel;
+import ru.nsu.ccfit.bogush.factory.Supplier;
 
 import javax.swing.*;
+import java.awt.event.WindowAdapter;
+import java.awt.event.WindowEvent;
 
 public class FactoryView extends JPanel {
 	private JPanel mainPanel;
@@ -16,7 +19,19 @@ public class FactoryView extends JPanel {
 		this.model = model;
 		JFrame frame = new JFrame("Car Factory");
 		frame.setContentPane(mainPanel);
-		frame.setDefaultCloseOperation(WindowConstants.EXIT_ON_CLOSE);
+		frame.setDefaultCloseOperation(WindowConstants.DISPOSE_ON_CLOSE);
+		frame.addWindowListener(new WindowAdapter() {
+			@Override
+			public void windowClosed(WindowEvent e) {
+				model.getStore().stop();
+				model.getEngineSupplier().getThread().interrupt();
+				model.getBodySupplier().getThread().interrupt();
+				for (Supplier s : model.getAccessorySuppliers()) {
+					s.getThread().interrupt();
+				}
+				model.getCarFactory().getThreadPool().stop();
+			}
+		});
 		frame.setResizable(false);
 		frame.setLocation(0, 0);
 		frame.pack();
