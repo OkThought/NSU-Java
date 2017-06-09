9c5eb7f92d9aa13dd9eed3df2d4549dbfb479fce
RoyPy <bogush.vano@gmail.com>
2017-06-09 20:08:47 +0700

    Implement User

--- Chat/src/ru/nsu/ccfit/bogush/User.java
+++ Chat/src/ru/nsu/ccfit/bogush/User.java
@@ -1,21 +1,22 @@
 package ru.nsu.ccfit.bogush;
 
-import org.apache.logging.log4j.LogManager;
-import org.apache.logging.log4j.Logger;
-
-import java.net.Socket;
-
 public class User {
-	private static final Logger logger = LogManager.getLogger();
+	private String nickname;
 
-	private Server server;
-	private Socket socket;
-	private String name;
+	public User(String nickname) {
+		this.nickname = nickname;
+	}
 
-	public User(Server server, Socket socket) {
-		logger.traceEntry("User");
-		this.server = server;
-		this.socket = socket;
-		logger.traceExit("User", null);
+	public String getNickname() {
+		return nickname;
+	}
+
+	public void setNickname(String nickname) {
+		this.nickname = nickname;
+	}
+
+	@Override
+	public String toString() {
+		return getNickname();
 	}
 }
