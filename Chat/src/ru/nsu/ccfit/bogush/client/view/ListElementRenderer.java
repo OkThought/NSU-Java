de915bf3b9476a6d5a7fe32027257fd81c41d278
RoyPy <bogush.vano@gmail.com>
2017-06-16 10:16:12 +0700

    Make FORMAT constant field

--- Chat/src/ru/nsu/ccfit/bogush/client/view/ListElementRenderer.java
+++ Chat/src/ru/nsu/ccfit/bogush/client/view/ListElementRenderer.java
@@ -4,12 +4,18 @@ import javax.swing.*;
 import java.awt.*;
 
 class ListElementRenderer extends DefaultListCellRenderer {
+	private static final String FORMAT =
+			"<html>" +
+					"<body WIDTH=%d>" +
+							"%s" +
+					"</body>" +
+			"</html>";
 	private int width = super.getWidth();
 
 	@Override
 	public Component getListCellRendererComponent(JList list, Object value,
 	                                              int index, boolean isSelected, boolean cellHasFocus) {
-		String text = String.format("<html><body WIDTH=%d>%s", width, value.toString());
+		String text = String.format(FORMAT, width, value.toString());
 		return super.getListCellRendererComponent(list, text, index, isSelected, cellHasFocus);
 	}
 
